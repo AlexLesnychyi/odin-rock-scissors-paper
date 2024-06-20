fedafd9c264515e73bc7b0a10569d5017a804068
Alex <172589611+AlexLesnychyi@users.noreply.github.com>
2024-06-20 23:33:22 +0300

    Rps test

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-function getHumanChoice() {
-    let userChoice = prompt("Type your choice!")
-      if (userChoice === null || userChoice === undefined) {
+function getHumanChoice(choice) {
+    
+      if (choice === null || choice === undefined) {
         alert("No input provided. Try again.");
         return null;
     }
-    let text= userChoice.toLowerCase()
+    let text= choice.toLowerCase()
     if (text === "rock") {
         return "Rock";
     } else if (text === "scissors") {
@@ -44,26 +44,87 @@ function playRound(humanChoice, computerChoice) {
         return `Congratulations Computer wins! ${computerChoice} beats ${humanChoice}`;
     }
 }
+function updaTedResults(message) {
 
-
-function playGame(num) {
-    for (i = 0; i < num; i++) {
-      let humanChoice = getHumanChoice();
-        if (humanChoice === null) {
-            console.log("Game cancelled.");
-            return;
-        }
-        let computerChoice = getComputerChoice()
-        let result = playRound(humanChoice, computerChoice)
-        console.log(result)
-        console.log(`"computerScore" = ${computerScore}; "humanScore" = ${humanScore}`)
-    }
-    if (computerScore > humanScore) {
-        return `"Computer win! "${computerScore} ":" ${humanScore}`;
-    } else if (humanScore > computerScore)  {
-         return `"Human win! "${humanScore} ":" ${computerScore}`;
-    } else return "It's a Tie!"
-       
-   
+    resultD.textContent = message + ` HumanScore= ${humanScore}  ComputerScore ${computerScore}`;
 }
+function checkWinner() {
+    if (humanScore >= 5) {
+        alert("Human wins the game!");
+        resetGame();
+    } else if (computerScore >= 5) {
+        alert("Computer wins the game!");
+        resetGame();
+    }
+}
+
+ const container = document.querySelector("#container");
+const rockBtn = document.createElement("button")
+rockBtn.textContent = "Rock"
+rockBtn.style.padding = "15px 10px"
+rockBtn.style.backgroundColor = "red"
+rockBtn.style.width= "100px"
+const scsBtn = document.createElement("button")
+scsBtn.textContent = "Scissors"
+scsBtn.style.padding = "15px 10px"
+scsBtn.style.backgroundColor = "green"
+scsBtn.style.width= "100px"
+const papBtn = document.createElement("button")
+papBtn.textContent = "Paper"
+papBtn.style.padding = "15px 10px"
+papBtn.style.backgroundColor = "blue"
+papBtn.style.width = "100px"
+const resultD = document.querySelector("#result")
+resultD.style.width = "300px"
+resultD.style.height = "100px"
+resultD.style.border = "3px solid"
+resultD.textContent= " Let's Begin!"
+
+
+container.append(rockBtn, scsBtn, papBtn, resultD)
+
+rockBtn.addEventListener('click', (e) => {
+    let choice = e.target.textContent
+    let humanChoice = getHumanChoice(choice);
+    let computerChoice = getComputerChoice();
+    let result = playRound(humanChoice, computerChoice);
+    updaTedResults(result)
+    checkWinner()
+    
+})
+scsBtn.addEventListener('click', (e) => {
+    let choice = e.target.textContent
+    let humanChoice = getHumanChoice(choice);
+    let computerChoice = getComputerChoice();
+    let result = playRound(humanChoice, computerChoice);
+     updaTedResults(result)
+ checkWinner()
+    })
+papBtn.addEventListener('click', (e) => {
+    let choice = e.target.textContent
+    let humanChoice = getHumanChoice(choice);
+    let computerChoice = getComputerChoice();
+    let result = playRound(humanChoice, computerChoice);
+    updaTedResults(result);
+    checkWinner()
+   })
+// function playGame(num) {
+//     for (i = 0; i < num; i++) {
+//       let humanChoice = getHumanChoice();
+//         if (humanChoice === null) {
+//             console.log("Game cancelled.");
+//             return;
+//         }
+//         let computerChoice = getComputerChoice()
+//         let result = playRound(humanChoice, computerChoice)
+//         console.log(result)
+//         console.log(`"computerScore" = ${computerScore}; "humanScore" = ${humanScore}`)
+//     }
+//     if (computerScore > humanScore) {
+//         return `"Computer win! "${computerScore} ":" ${humanScore}`;
+//     } else if (humanScore > computerScore)  {
+//          return `"Human win! "${humanScore} ":" ${computerScore}`;
+//     } else return "It's a Tie!"
+       
+// }
 
